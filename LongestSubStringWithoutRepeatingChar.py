6d0427a5b045d84947e90ad7dd57a1e3af456d97
Yousef-Zoumot <yousefzoumot@gmail.com>
2023-06-15 09:51:41 -0700

    Create LongestSubStringWithoutRepeatingChar.py

---
+++ LongestSubStringWithoutRepeatingChar.py
@@ -0,0 +1,52 @@
+"""
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+"""
+
+# O(n) Time Complexity for below solution
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        longest = 0
+        startingP = 0
+        dictionary = {}
+
+        for i in range(len(s)):
+            char = s[i]
+            index = dictionary.get(char)
+            if index is not None and index >= startingP:
+                length = i - startingP
+                startingP = index + 1
+                if length > longest:
+                    longest = length
+            dictionary[char] = i
+        return max(longest, (len(s) - startingP))
